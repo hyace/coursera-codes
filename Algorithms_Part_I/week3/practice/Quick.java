fdfb33674cccb46a1bc4f740d8992fbc2792fcb3
Cheung Hui <hyace@126.com>
2014-07-10 23:42:12 +0800

    update
    The original codes are wrong .
    Fix some boundaries.

--- Algorithms_Part_I/week3/practice/Quick.java
+++ Algorithms_Part_I/week3/practice/Quick.java
@@ -1,6 +1,7 @@
 /**
- * Quick Sort
- * 测试中如果数据集是顺序的，即使不大也会造成栈溢出，添加shuffle后测试
+ * Quick Sort 测试中如果数据集是顺序的，即使不大也会造成栈溢出，添加shuffle后测试 初始版本有问题，排序是错的，
+ * 修改了sort中的几个下标后好了，QuickSort各有各的实现方式，所以自己设置边界时要小心。
+ * 
  * @author Chyace
  * 
  */
@@ -8,20 +9,20 @@ public class Quick {
     public static void sort(Comparable[] a) {
         // shuffle之後不會再stackoverflow
         StdRandom.shuffle(a);
-        sort(a, 0, a.length);
+        sort(a, 0, a.length - 1);
     }
 
     private static void sort(Comparable[] a, int lo, int hi) {
         if (lo >= hi)
             return;
-        int i = lo, j = hi;
-        do {
+        int i = lo, j = hi + 1;
+        while (true) {
             while (++i < hi && less(a[i], a[lo]));
             while (--j > lo && less(a[lo], a[j]));
-            if (i < j) {
-                exch(a, i, j);
-            }
-        } while (i < j);
+            if (i >= j)
+                break;
+            exch(a, i, j);
+        }
         exch(a, lo, j);
         sort(a, lo, j - 1);
         sort(a, j + 1, hi);
